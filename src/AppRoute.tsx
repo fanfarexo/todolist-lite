1ea77b51b5d6041e18449119547d4395da5a0839
fanfarexo <fanfarexo@gmail.com>
2024-10-11 11:55:21 +0900

    chore: 라우팅 및 레이아웃 구성 업데이트

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import TodoList from './pages/TodoList';
+import AddTodo from './pages/AddTodo';
+import EditTodo from './pages/EditTodo';
+
 const AppRoute = () => {
-  return <div>AppRoute</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<TodoList />} />
+          <Route path='add' element={<AddTodo />} />
+          <Route path='edit/:id' element={<EditTodo />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default AppRoute;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,15 @@
+import { Outlet } from 'react-router';
+import Footer from './Footer';
+import Header from './Header';
+
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
